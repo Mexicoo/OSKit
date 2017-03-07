79576686064b717eecd54f081ba002ab049c47ab
Monster <zhangchunsheng@Monster-3.local>
2017-03-07 15:30:18 +0800

    Add the project

---
+++ OSKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OSKit
+//
+//  Created by 张春生 on 2017/3/7.
+//  Copyright © 2017年 Peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
